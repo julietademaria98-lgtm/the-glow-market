e6dc39b348a7d05baa5ade7e6a48ebda634aaf65
julietademaria98-lgtm <julietademaria98@gmail.com>
2026-07-15 10:21:25 -0300

    Update NavbarWrapper.tsx

--- the-glow-market/components/layout/NavbarWrapper.tsx
+++ the-glow-market/components/layout/NavbarWrapper.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import { usePathname } from 'next/navigation'
 import Navbar from './Navbar'
 import Footer from './Footer'
@@ -8,6 +7,8 @@ import CartDrawer from './CartDrawer'
 export default function NavbarWrapper() {
   const pathname = usePathname()
   if (pathname.startsWith('/admin')) return null
+  if (pathname.startsWith('/mi-curso/curso/')) return null
+  if (pathname.match(/^\/mi-curso\/[^/]+$/)) return null
   return (
     <>
       <Navbar />
@@ -19,5 +20,7 @@ export default function NavbarWrapper() {
 export function FooterWrapper() {
   const pathname = usePathname()
   if (pathname.startsWith('/admin')) return null
+  if (pathname.startsWith('/mi-curso/curso/')) return null
+  if (pathname.match(/^\/mi-curso\/[^/]+$/)) return null
   return <Footer />
 }
